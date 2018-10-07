92f6fb901afee74c3ee788976049003a92afb381
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-10-08 02:48:54 +0900

    Add OGP

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,12 +21,22 @@
     }
   })
   export default class extends Vue {
-    head() {
+    head(args) {
+      const url = `https://animare.cafe${this.$route.fullPath}`;
+      const title = this.$t('global.title')
+      const description = this.$t('global.description')
+      const mainVisualPath = require('~/assets/images/main-visual.jpg')
+
       return {
         htmlAttrs: {
           lang: this.$i18n.locale
         },
         link: [
+          {
+            href: url,
+            hid: 'canonical',
+            rel: 'canonical'
+          },
           {
             href: 'https://fonts.googleapis.com/css?family=Roboto|Noto+Sans+JP:400,700',
             rel: 'stylesheet'
@@ -37,9 +47,49 @@
           {
             content: 'initial-scale=1.0,width=device-width',
             name: 'viewport'
+          },
+          {
+            content: 'website',
+            hid: 'og:type',
+            property: 'og:type'
+          },
+          {
+            content: title,
+            hid: 'og:title',
+            property: 'og:title'
+          },
+          {
+            content: mainVisualPath,
+            hid: 'og:url',
+            property: 'og:image'
+          },
+          {
+            content: url,
+            hid: 'og:url',
+            property: 'og:url'
+          },
+          {
+            content: 'summary_large_image',
+            hid: 'twitter:card',
+            name: 'twitter:card'
+          },
+          {
+            content: title,
+            hid: 'twitter:title',
+            name: 'twitter:title'
+          },
+          {
+            content: description,
+            hid: 'twitter:description',
+            name: 'twitter:description'
+          },
+          {
+            content: mainVisualPath,
+            hid: 'twitter:image',
+            name: 'twitter:image'
           }
         ],
-        title: this.$t('global.title')
+        title
       }
     }
   }
