652add025ffd2340623fc3b7bcb91a371a936b60
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-10-12 10:45:36 +0900

    Fix...

--- store/articles.ts
+++ store/articles.ts
@@ -10,7 +10,7 @@ export const mutations = {
 
 export const actions = {
   async fetch({ commit, state }) {
-    if (state.list.length > 0) return
+    if (state.list && state.list.length > 0) return
 
     const { default: articles } = await import('~/static/api/articles.json')
 
