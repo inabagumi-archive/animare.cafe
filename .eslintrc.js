ee1d9ac73503708bd8b89f0b1c3f2b17a0a5ee79
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-11-26 21:06:30 +0900

    chore(deps): bump eslint-plugin-vue v5.0.0-beta.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
     'vue/html-self-closing': [
       'error',
       {
