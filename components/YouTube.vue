3ee84009dac1472e95ebdc467c5c4cc6a8c9d213
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-02-20 21:56:43 +0900

    feat(ui): add video

---
+++ components/YouTube.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="youtube">
+    <iframe
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      class="youtube__frame"
+      frameborder="0"
+      height="315"
+      :src="`https://www.youtube.com/embed/${id}`"
+      width="560"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {
+  @Prop({ required: true, type: String }) id!: string
+}
+</script>
+
+<style scoped>
+.youtube {
+  height: 0;
+  padding-top: 56.25%;
+  position: relative;
+  width: 100%;
+}
+
+.youtube__frame {
+  height: 100%;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100%;
+}
+</style>
