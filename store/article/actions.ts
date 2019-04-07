feb381ade59c18f11df000f0f0b7f0b0334aa9d0
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-04-08 00:43:55 +0900

    use articles API

--- store/article/actions.ts
+++ store/article/actions.ts
@@ -5,9 +5,9 @@ import { ArticleState } from '~/store/article/state'
 const actions: ActionTree<ArticleState, RootState> = {
   async fetchList({ commit, rootState }) {
     const { locale } = (rootState as any).i18n
-    const {
-      default: articles
-    } = await import(`~/static/api/${locale}/articles.json`)
+    const uri = `${rootState.apiURI}/${locale}/articles`
+    const response = await fetch(uri)
+    const articles = await response.json().catch(() => null)
 
     commit('set', { articles })
   }
